95292a262ef78f5cd12de600ff6f682ebe3cc94d
Halo <doresimon@163.com>
2019-09-17 19:32:57 +0800

    update sgx

--- mesatee/run.sh
+++ mesatee/run.sh
@@ -1,1 +1,21 @@
-docker run --rm -it --device /dev/isgx --device /dev/mei0 sgx
+### on host machine ###
+# install icls
+sudo alien --scripts iclsClient-1.45.449.12-1.x86_64.rpm
+sudo dpkg -i iclsclient_1.45.449.12-2_amd64.deb
+
+# install jhi service 
+sudo apt install -y cmake pkg-config uuid-dev libxml2-dev libsystemd-dev
+
+git clone https://github.com/01org/dynamic-application-loader-host-interface.git
+cd dynamic-application-loader-host-interface
+cmake .
+make
+sudo make install
+
+# start container
+docker run --rm -it --device /dev/isgx --device /dev/mei0 -v /var/run/aesmd/aesm.socket:/var/run/aesmd/aesm.socket sgx
+
+
+##### failed sample 
+# 1. RemoteAttestation
+# 2. SampleEnclavePCL
