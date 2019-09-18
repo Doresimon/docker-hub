db43a71a81145450f1550061ba8f7544757219fb
Halo <doresimon@163.com>
2019-09-18 14:54:42 +0800

    .

--- SGX/run.sh
+++ SGX/run.sh
@@ -1,10 +1,10 @@
 ### on host machine ###
 # install icls
-sudo alien --scripts iclsClient-1.45.449.12-1.x86_64.rpm
-sudo dpkg -i iclsclient_1.45.449.12-2_amd64.deb
+# sudo alien --scripts iclsClient-1.45.449.12-1.x86_64.rpm
+# sudo dpkg -i iclsclient_1.45.449.12-2_amd64.deb
 
-# install jhi service 
-sudo apt install -y cmake pkg-config uuid-dev libxml2-dev libsystemd-dev
+# # install jhi service 
+# sudo apt install -y cmake pkg-config uuid-dev libxml2-dev libsystemd-dev
 
 # git clone https://github.com/01org/dynamic-application-loader-host-interface.git
 # cd dynamic-application-loader-host-interface
